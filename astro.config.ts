335e98167b40d494a729a8c793d4e42b19c4d5d9
Victor Yamunaque <vyamunaque@pop-os.localdomain>
2024-10-28 17:09:36 -0500

    Fix asset dir

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   site: 'https://vyamunaque99.github.io',
   base: 'is-it-on-sci-hub-landing',
   integrations: [
